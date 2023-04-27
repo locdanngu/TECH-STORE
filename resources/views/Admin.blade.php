87523138a430901518f9ecf9bca89ba09bfe07a7
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-04-27 14:37:54 +0700

    up

--- resources/views/Admin.blade.php
+++ resources/views/Admin.blade.php
@@ -389,13 +389,13 @@
                                                     <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
                                                         {{ $revenue->ordernumber}}</div>
                                                 </div>
-                                                <div class="col">
+                                                <!-- <div class="col">
                                                     <div class="progress progress-sm mr-2">
                                                         <div class="progress-bar bg-info" role="progressbar"
                                                             style="width: 50%" aria-valuenow="50" aria-valuemin="0"
                                                             aria-valuemax="100"></div>
                                                     </div>
-                                                </div>
+                                                </div> -->
                                             </div>
                                         </div>
                                         <div class="col-auto">
@@ -418,7 +418,7 @@
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $countuser }}</div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                            <i class="fa fa-user fa-2x text-gray-300"></i>
                                         </div>
                                     </div>
                                 </div>
