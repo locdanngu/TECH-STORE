1e0b75c9deecfc5d329600ecb3c1a6b893e82753
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-05-12 08:20:12 +0700

    up

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -38,7 +38,7 @@ class MessageController extends Controller
                     <p class="fixtxt" style="color:White;font-weight:bold">' . $useradmin->name . '</p>
                 </div>
                 <hr class="hr">
-                <div class="noidungchat" style="padding:2em;height:70vh">';
+                <div class="noidungchat" style="padding:2em;height:70vh;overflow-x: auto">';
         foreach ($messages as $message) {
             if ($message->sender_id == $user->id) {
                 $html .= '<div style="display:flex;justify-content: end;">';
@@ -48,20 +48,22 @@ class MessageController extends Controller
                 $html .= '</div>';
                 $html .= '<img src="' .$user->avatar. '" class="img">';
                 $html .= '</div>';
+                $html .= '<br>';
             } else {
                 $html .= '<div style="display:flex">';
                 $html .= '<img src="'. $message->sender->avatar .'" class="img">';
                 $html .= '<div style="display:flex;flex-direction:column;margin-left:1em">';
-                $html .= '<span style="color:white;border:1px solid white;padding:.25em;border-radius:1em;width: fit-content;">'.$message->message.'</span>';
+                $html .= '<span style="color:white;border:1px solid white;padding:.5em;border-radius:1em;width: fit-content;">'.$message->message.'</span>';
                 $html .= '<span style="color:white;font-size:10px">(Send at:'.$message->created_at.')</span>';
                 $html .= '</div>';
                 $html .= '</div>';
+                $html .= '<br>';
             }
         }
         $html .= '</div>
                         <div style="display:flex;justify-content:space-between;padding:1em 1em">
-                            <textarea class="textarea"></textarea>
-                            <button class="sendbtn">Send</button>
+                            <textarea class="textarea" name="messagecontent"></textarea>
+                            <button class="sendbtn" id="buttonsend">Send</button>
                         </div>
                     </div>';
 
