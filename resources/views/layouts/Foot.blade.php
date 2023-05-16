6b3de73f106221aa64e602a3ca699662b423df9f
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-05-16 16:20:24 +0700

    up

--- resources/views/layouts/Foot.blade.php
+++ resources/views/layouts/Foot.blade.php
@@ -9,7 +9,7 @@
         <p class="headfoot">LINK</p>
         <a href="{{ route('login.page') }}" class="bodyfoot link">Watch more</a>
         <a href="{{ route('login.page') }}" class="bodyfoot link">Login page</a>
-        <a href="{{ route('signup.page') }}" class="bodyfoot link">Register ppage</a>
+        <a href="{{ route('signup.page') }}" class="bodyfoot link">Register page</a>
     </div>
     <div class="foot">
         <p class="headfoot">POPULAR</p>
@@ -20,12 +20,12 @@
     <div class="foot">
         <p class="headfoot">CONTACT US</p>
         <div class="fix">
-            <a href="#" class="bodyfoot link"><i class="bi bi-facebook"></i></a>
+            <a href="https://www.facebook.com/tranvanloc2001" class="bodyfoot link"><i class="bi bi-facebook"></i></a>
             <a href="#" class="bodyfoot link"><i class="bi bi-twitter"></i></a>
         </div>
     </div>
 </footer>
 <div class="morefoot">
-    <p class="bodyfoot">Copyright © 2011-2023 Technology store</p>
+    <p class="bodyfoot">Copyright © 2023-2024 Technology store</p>
     <p class="bodyfoot">Dự án Laravel</p>
 </div>
