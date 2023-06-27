744b060af3e6a95ad270a950c5c1a497ef225b10
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-06-27 14:49:55 +0700

    up

--- resources/views/Test.blade.php
+++ resources/views/Test.blade.php
@@ -1,27 +1,56 @@
 <!DOCTYPE html>
-<head>
-  <title>Pusher Test</title>
-  <script src="https://js.pusher.com/5.0/pusher.min.js"></script>
-  <script>
+<html>
 
+<head>
+    <title>Pusher Test</title>
+    <script src="https://js.pusher.com/5.0/pusher.min.js"></script>
+</head>
+
+<body>
+    <h1>Pusher Test</h1>
+    <p>
+        Try publishing an event to channel <code>my-channel</code>
+        with event name <code>my-event</code>.
+    </p>
+
+    <div id="message-container"></div>
+
+
+    <textarea id="input-textarea"></textarea>
+    <button onclick="sendMessage()">Send</button>
+
+    <script>
     // Enable pusher logging - don't include this in production
     Pusher.logToConsole = true;
 
     var pusher = new Pusher('ea12aec9241c98d61eef', {
-      cluster: 'ap1',
-      forceTLS: true
+        cluster: 'ap1',
+        forceTLS: true
     });
 
-    var channel = pusher.subscribe('my-channel');
+    var messageContainer = document.getElementById('message-container');
+
+    var channel = pusher.subscribe('normal-cow-680');
     channel.bind('my-event', function(data) {
-      alert(JSON.stringify(data));
+        messageContainer.innerHTML = data;
     });
-  </script>
-</head>
-<body>
-  <h1>Pusher Test</h1>
-  <p>
-    Try publishing an event to channel <code>my-channel</code>
-    with event name <code>my-event</code>.
-  </p>
+
+    channel.bind('pusher:subscription_succeeded', function(data) {
+        console.log("Subscription succeeded:", data);
+    });
+
+
+    // function sendMessage() {
+    //     var inputTextarea = document.getElementById('input-textarea');
+    //     var message = inputTextarea.value;
+
+    //     channel.trigger('client-my-event', {
+    //         message: message
+    //     });
+
+    //     inputTextarea.value = '';
+    // }
+    </script>
 </body>
+
+</html>
